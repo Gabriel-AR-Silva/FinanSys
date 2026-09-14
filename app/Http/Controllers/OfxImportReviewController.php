f90ac30e4c147550969b4937a0256855d2f84331
Gabriel Augusto Ribeiro da Silva <117488579+Gabriel-AR-Silva@users.noreply.github.com>
2026-09-13 21:39:41 -0300

    feat: add OFX review controller scaffold

---
+++ app/Http/Controllers/OfxImportReviewController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class OfxImportReviewController extends Controller
+{
+    public function update(Request $request): RedirectResponse
+    {
+        return back();
+    }
+}
