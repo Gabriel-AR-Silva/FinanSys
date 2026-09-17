8d3c5d3cc81d3332691c623f78d3b0c6e0a4935e
Gabriel Augusto Ribeiro da Silva <117488579+Gabriel-AR-Silva@users.noreply.github.com>
2026-09-17 08:42:23 -0300

    style(ci): formatar User sem alterar comportamento

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,13 +18,40 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
-    public function accounts(): HasMany { return $this->hasMany(Account::class); }
-    public function pockets(): HasMany { return $this->hasMany(Pocket::class); }
-    public function ledgerEntries(): HasMany { return $this->hasMany(LedgerEntry::class); }
-    public function categories(): HasMany { return $this->hasMany(Category::class); }
-    public function socialIdentities(): HasMany { return $this->hasMany(SocialIdentity::class); }
-    public function financialEvaluations(): HasMany { return $this->hasMany(FinancialEvaluation::class); }
-    public function internalAlerts(): HasMany { return $this->hasMany(InternalAlert::class); }
+    public function accounts(): HasMany
+    {
+        return $this->hasMany(Account::class);
+    }
+
+    public function pockets(): HasMany
+    {
+        return $this->hasMany(Pocket::class);
+    }
+
+    public function ledgerEntries(): HasMany
+    {
+        return $this->hasMany(LedgerEntry::class);
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function socialIdentities(): HasMany
+    {
+        return $this->hasMany(SocialIdentity::class);
+    }
+
+    public function financialEvaluations(): HasMany
+    {
+        return $this->hasMany(FinancialEvaluation::class);
+    }
+
+    public function internalAlerts(): HasMany
+    {
+        return $this->hasMany(InternalAlert::class);
+    }
 
     public function sendPasswordResetNotification($token): void
     {
@@ -33,6 +60,9 @@ class User extends Authenticatable
 
     protected function casts(): array
     {
-        return ['email_verified_at' => 'datetime', 'password' => 'hashed'];
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
