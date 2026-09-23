4d34c5042a990da312fdb5ce5fed39e77066894e
Gabriel Augusto Ribeiro da Silva <117488579+Gabriel-AR-Silva@users.noreply.github.com>
2026-09-23 08:07:36 -0300

    refactor: run Inertia recovery outside Inertia middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\RecoverStaleInertiaNavigation;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'feature' => EnsureFeatureEnabled::class,
+        ]);
+
+        $middleware->web(prepend: [
+            RecoverStaleInertiaNavigation::class,
         ]);
 
         $middleware->web(append: [
